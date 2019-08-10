628d2fbba1ec304894b4456c6de622c4db0d1862
AzureFx <i@azurefx.name>
2019-08-11 02:36:40 +0800

    WIP: V5Packet

---
+++ src/V5Packet.jl
@@ -0,0 +1,59 @@
+module Packet
+
+using UUIDs
+
+const MAGIC = 0x2b2b3556
+const REPLY_MASK = 0x1
+
+struct V5Packet
+    magic::UInt32
+    requestid::UUID
+    flags::UInt8
+    length::UInt16
+    payload::Array{UInt8}
+end # struct
+
+function V5Packet(payload::Array{UInt8}, replyto::UUID)::V5Packet
+    len = length(payload)
+    len > typemax(UInt16) && throw(ArgumentError("Payload too large: $len"))
+    V5Packet(replyto, 0, len, payload)
+end
+
+getflag(pkt, mask) = (pkt.flags & mask) != 0
+setflag(pkt, mask, x) = x ? pkt.flags |= mask : pkt.flags &= ~mask
+
+function Base.getproperty(pkt::V5Packet, sym)
+    if sym == :REPLY
+        getflag(pkt, REPLY_MASK)
+    else
+        getfield(pkt, sym)
+    end
+end
+
+function Base.setproperty!(pkt::V5Packet, sym, x)
+    if sym == :REPLY
+        setflag(pkt, REPLY_MASK, x)
+    else
+        setfield!(pkt, sym, x)
+    end
+end
+
+function Base.read(io::IO, ::Type{V5Packet})::V5Packet
+    magic = read(io, UInt32)
+    magic == MAGIC || throw(ErrorException("Invalid magic $magic"))
+    requestid = read(io, UInt128) |> UUID
+    flags = read(io, UInt8)
+    len = read(io, UInt16)
+    payload = read(io, length)
+    V5Packet(magic, requestid, flags, len, payload)
+end
+
+function Base.write(io::IO, x::V5Packet)
+    nb = write(io, x.magic)
+    nb += write(io, x.requestid)
+    nb += write(io, x.flags)
+    nb += write(io, x.length)
+    nb += write(io, x.payload)
+end
+
+end # module
--- src/V5RPC.jl
+++ src/V5RPC.jl
@@ -2,6 +2,8 @@ module V5RPC
 
 (filename->include(joinpath(@__DIR__, "..", "deps", "julia_pb", filename))).(["DataStructures_pb.jl","Events_pb.jl","API_pb.jl"])
 
+include("V5Packet.jl")
+
 abstract type Strategy end
 
 export Strategy
